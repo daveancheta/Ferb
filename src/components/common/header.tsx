aab6d82e6b6aa16b7c0c1dc05b9ca8ef489138ad
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-21 00:15:56 +0800

    style: fix responsiveness

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -14,14 +14,14 @@ function Header() {
     dark:bg-background/90 border-b-2 m-0">
             <div className="flex justify-between sm:mx-30 mx-10 py-2 items-center">
                 <div className="flex flex-row items-center gap-8">
-                    <h1 className="font-extrabold uppercase 
-                    purple-ferb">
-                        <Link href="/">
-                            <HyperText className="text-2xl">ferb</HyperText>
+                    <h1 className="font-extrabold uppercase">
+                        <Link href="/" className="flex flex-row items-center">
+                        <img className="w-10 h-10" src="/ferb-logo.png" alt="" />
+                            <HyperText className="text-2xl sm:flex hidden">ferb</HyperText>
                         </Link>
                     </h1>
 
-                    <div className="sm:flex hidden flex-row gap-2 text-sm items-center">
+                    <div className="lg:flex flex-row hidden gap-2 text-sm items-center">
                         <Link href="" className={cn(NavHover)}>Documentation</Link>
                         <Link href="https://nextjs.org/" className={cn(NavHover)}>Next.js</Link>
                         <Link href="https://zustand-demo.pmnd.rs/" className={cn(NavHover)}>Zustand</Link>
@@ -31,7 +31,7 @@ function Header() {
                 </div>
 
                 <div className="flex items-center gap-4">
-                    <InputGroup className="rounded-full sm:flex hidden">
+                    <InputGroup className="rounded-full lg:flex hidden">
                         <InputGroupInput placeholder="Search Documentation..." />
                         <InputGroupAddon>
                             <Search />
